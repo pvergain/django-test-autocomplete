bbacb8275667c076480ea79a75dda7a5ac1defb6
Patrick Vergain <pvergain@gmail.com>
2016-10-20 14:44:37 +0200

    add EasyAutocomplete asset files (Javascript + CSS)

--- projet_ajax/projet_ajax/settings.py
+++ projet_ajax/projet_ajax/settings.py
@@ -156,10 +156,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = 'staticfiles'
-STATICFILES_DIRS = (
-            os.path.join(BASE_DIR, 'static'),
-            )
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 AJAX_LOOKUP_CHANNELS = {
     # simplest way, automatically construct a search channel by passing a dict
