d214f940a9ca7110bf595aef6be9c9496037712a
Patrick Vergain <pvergain@gmail.com>
2016-10-21 08:56:08 +0200

    add httpie https://httpie.org/

--- projet_ajax/projects/urls.py
+++ projet_ajax/projects/urls.py
@@ -13,13 +13,16 @@ urlpatterns = [
          name='project_update'),
 
      # calls by jquery-ui autocomplete (AJAX calls)
+     # http://127.0.0.1:8004/projects/champion_get_json/?term=a
      url(r'^champion_get_json/$',
          ChampionAutoCompleteView.as_view(),
          name='champion_get_json'),
 
-    url(
-        r'^projet_autocomplete/$',
+     # calling example:
+     # http://127.0.0.1:8004/projects/project_autocomplete/?q=a
+     url(
+        r'^project_autocomplete/$',
         ProjectAutocompleteView.as_view(),
-        name='projet_autocomplete',
-    ),
+        name='project_autocomplete',
+     ),
 ]
